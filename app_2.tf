9c1b2df7237338d57de83433e6bbc2431fc91e09
yinkadevops <yinkadevops@gmail.com>
2022-09-14 13:22:05 -0500

    modified app_2

--- app_2.tf
+++ app_2.tf
@@ -20,3 +20,29 @@ resource "aws_vpc" "maine" {
   }
 } 
 
+resource "aws_vpc" "maine" {
+   cidr_block = "10.0.0.0/16"
+   tags = {
+    "Name" = "main_VPC"
+   }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.maine.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_subnet" "main1" {
+  vpc_id     = "${aws_vpc.maine.id}"
+  cidr_block = "10.0.0.0/25"
+  map_public_ip_on_launch = "false"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet"
+  }
+}
